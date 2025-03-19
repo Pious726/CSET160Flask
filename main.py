14e9ec652484e56830467d12eea28393b5f3ff01
Tyler M. <tjmcdowell726@gmail.com>
2025-03-19 07:55:01 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+
+app = Flask(__name__)
+conn_str = "mysql://root:cset155@localhost/boatdb"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/boats')
+def boats():
+    boats = conn.execute(text('select * from boats')).all()
+    return render_template('boats.html', boats = boats[:10])
+
+@app.route('/boatCreate', methods=["GET"])
+def getBoat():
+    return render_template('boat_create.html')
+
+@app.route('/boatCreate', methods=["POST"])
+def createBoat():
+    try:
+        conn.execute(text('insert into boats values(:id, :name, :type, :owner_id, :rental_price)'), request.form)
+        return render_template('boat_create.html', error = None, success = "Successful")
+    except:
+        return render_template('boat_create.html', error = "Failed", success = None)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
